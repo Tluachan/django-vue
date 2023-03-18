19b2d5f9a7c7f21b002217d54c8ed664eeb6b3a7
Tamonwan Luachan <2806502L@student.gla.ac.uk>
2023-03-18 17:06:48 +0000

    review serializer

--- djackets_django/product/serializers.py
+++ djackets_django/product/serializers.py
@@ -1,12 +1,41 @@
 from rest_framework import serializers
 
-from .models import Category, Product
+from .models import Category, Product, Review
 
 from djoser.serializers import UserCreateSerializer
 from django.contrib.auth import get_user_model
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    shop_id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+
+    class Meta:
+        model = Review
+        fields = (
+            'id',
+            'user_id',
+            'shop_id',
+            'content',
+            'rating',
+            'datetime'
+        )
+
+    def create(self, validated_data):
+        # Get the user and product objects from the validated data
+        user = self.context["request"].user
+        product = self.context["product"]
+
+        # Create the review object with the validated data and user/product objects
+        review = Review.objects.create(
+            user_id=user.id,
+            shop_id=product.id,
+            content=validated_data["content"],
+            rating=validated_data["rating"],
+        )
+
+        return review    
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
